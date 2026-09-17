83a8d637c2b5e7fa414595be78022c6840d00c70
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 07:37:50 +0800

    fix: bust cache for D-Zone filter repairs

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.11";
+  const assetVersion="20260918.12";
     const urls=[
       "morimens-v03/part1.b64",
       "morimens-v03/part2a.b64",
