0c1cc4b6639b7f56f324bf2bbc2d945550df6519
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 07:12:02 +0800

    修复线上脚本缓存与融灾数据解码

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.6";
+  const assetVersion="20260918.10";
     const urls=[
       "morimens-v03/part1.b64",
       "morimens-v03/part2a.b64",
