12b6e12aa2a5e1fd8c425c077baeb82c1bf0f5c6
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 07:43:40 +0800

    feat: refresh assets for character expansion insights

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.12";
+  const assetVersion="20260918.13";
     const urls=[
       "morimens-v03/part1.b64",
       "morimens-v03/part2a.b64",
