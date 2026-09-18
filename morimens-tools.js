67372df756c3c272c68c1c6525bd581e9d81d3f4
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 14:08:21 +0800

    更新融灾榜单资源缓存版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.18";
+  const assetVersion="20260918.19";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
