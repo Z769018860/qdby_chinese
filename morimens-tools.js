362a9844f0cbb851abcbcda0dfb12896d85a0804
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 19:41:47 +0800

    修复标题图加载并更新缓存版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.25";
+  const assetVersion="20260918.26";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
