7c956828af83291380b792a9c9b95215bd7cc1ed
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 14:36:58 +0800

    更新中英文界面资源版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.22";
+  const assetVersion="20260918.23";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
