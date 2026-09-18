264ee45b2eea64fccaac2c62335ff73dab2d2385
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 21:44:45 +0800

    重制每日签档案式界面
    参考命轮档案图重做每日签排版、纸张框体与字体样式。

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.41";
+  const assetVersion="20260918.42";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
