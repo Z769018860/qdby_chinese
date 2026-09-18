d900e17ca9be34bc3aa0aa99931deb8b40199a83
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 19:48:22 +0800

    更新每日运势资源缓存版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.26";
+  const assetVersion="20260918.27";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
