e30519f4a906ecebf2ac24664881cd0783309feb
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 08:13:41 +0800

    feat: refresh assets for WWKB demo presentation

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.14";
+  const assetVersion="20260918.15";
     const urls=[
       "morimens-v03/part1.b64",
       "morimens-v03/part2a.b64",
