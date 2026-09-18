1779f2c5206538c13a8a69aafc631e046f28def2
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 14:29:13 +0800

    更新忘忘看报资源缓存版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.21";
+  const assetVersion="20260918.22";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
