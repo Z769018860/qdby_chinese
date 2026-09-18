f7ace8a625c85341e81f9f736567a6cd0c2f27bc
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 21:38:51 +0800

    增加主题化签词并左对齐命轮

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.39";
+  const assetVersion="20260918.41";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
