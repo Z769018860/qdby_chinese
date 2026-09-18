4654653debe6f32071b2cebff2e7a90c55689d23
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-18 14:25:23 +0800

    更新中文名称与Wiki链接缓存版本

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260918.20";
+  const assetVersion="20260918.21";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
