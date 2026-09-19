5a92b704b2595e2e7fa6410adc744ca0c841847d
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 00:44:56 +0800

    刷新命轮展开排版缓存

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.62";
+  const assetVersion="20260919.63";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
