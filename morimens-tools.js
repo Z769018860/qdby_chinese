5178e493f7de383acd97d0954b77d7419d003636
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-19 23:39:24 +0800

    刷新融灾榜单切换与日期缓存

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.56";
+  const assetVersion="20260919.57";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
