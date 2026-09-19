f4fdcc63f3522a7f6312d8a09d0cdfa8a5fb0609
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-19 20:53:25 +0800

    chore: refresh covenant icon assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.53";
+  const assetVersion="20260919.54";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
