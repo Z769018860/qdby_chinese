a1c830a55a1685d66dc1c7c64fdc4e8f4a627e29
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 00:38:18 +0800

    刷新造物筛选与钥令图标缓存

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.61";
+  const assetVersion="20260919.62";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
