2e32bbf6e6f366260d2d3c1bc289796690e3e245
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-19 23:16:00 +0800

    刷新旧版命轮与配队渲染缓存

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.54";
+  const assetVersion="20260919.55";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
