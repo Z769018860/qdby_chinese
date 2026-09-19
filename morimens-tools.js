7b21b2e741ad753da7c8d8a4c4c8ea2e4e002d1b
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-19 09:50:39 +0800

    Restore CG backgrounds for expanded legacy charts

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.49";
+  const assetVersion="20260919.50";
     const urls=[
       "morimens-v03/part1.b64",
       "morimens-v03/part2a.b64",
