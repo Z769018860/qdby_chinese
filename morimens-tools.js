4cf60cfbec245026fee645e93a7f68f89c087a49
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 07:14:09 +0800

    chore: refresh Love Rhythm assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260920.71";
+  const assetVersion="20260920.72";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
