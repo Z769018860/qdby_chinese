077cd9578c10f7973876f2f48df9236a450ddebc
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 06:34:30 +0800

    chore: load Love Rhythm module

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260919.64";
+  const assetVersion="20260920.71";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
