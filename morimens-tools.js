543f5ca7518412ded974d17fd7323f009a218ce5
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 07:37:30 +0800

    chore: bump calculator formula cache version

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.81";
+  const assetVersion="20260921.82";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
