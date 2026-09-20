1fa8feefe3df756785a3d27c81d1aed584c459e3
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 19:30:13 +0800

    chore: refresh Wangwang Report assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260920.76";
+  const assetVersion="20260920.77";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
