04ca589309a16ef2ef798abc6dd26f78fbd1a8dc
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 19:27:21 +0800

    chore: refresh Waline emoji module cache

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260920.75";
+  const assetVersion="20260920.76";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
