f064a01deb3d0adcd9d263932bea8ab5e7e4d683
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 07:56:59 +0800

    chore: bump progression calculator cache version

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.84";
+  const assetVersion="20260921.85";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
