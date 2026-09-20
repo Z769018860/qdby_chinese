37d0e6d5b1efb5472f2abd4dac782570c64f8380
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-20 19:55:54 +0800

    chore: refresh Wangwang Report emoji sizing

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260920.77";
+  const assetVersion="20260920.78";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
