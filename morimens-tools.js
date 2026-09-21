1f6bea8cb52e6941ecd3c00c2445ce891a68a643
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 22:27:05 +0800

    chore: refresh simplified damage calculator

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.152";
+  const assetVersion="20260921.153";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
