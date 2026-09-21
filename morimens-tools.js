c9c3f7a05a2fe9890e2a119d23ed7e13028af143
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 04:07:35 +0800

    chore: refresh localized calculator assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.9";
+  const assetVersion="20260922.10";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
