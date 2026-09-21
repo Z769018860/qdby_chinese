787df8ede3b5f18fb5df3125b98fff1a262f95a1
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 09:10:17 +0800

    chore: refresh calculator cache after damage fixes

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.99";
+  const assetVersion="20260921.100";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
