bba676d73ebf78912e4d8918223c41e2f903556a
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 20:19:20 +0800

    chore: refresh love ranking loader fix

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.142";
+  const assetVersion="20260921.143";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
