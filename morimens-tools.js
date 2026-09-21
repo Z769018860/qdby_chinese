bb3099bfdc2047c062e53ac352ae7f995cc60d9d
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 13:32:09 +0800

    chore: refresh calculator assets to 20260921.133

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.132";
+  const assetVersion="20260921.133";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
