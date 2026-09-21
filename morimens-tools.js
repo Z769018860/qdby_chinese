6d5d83b80a07b488a56116fec44c629ca5b8b782
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 06:50:27 +0800

    chore: bump localization cache version

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.17";
+  const assetVersion="20260922.18";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
