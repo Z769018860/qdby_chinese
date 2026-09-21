38f467a668483d987616f36c2c39bf30340eeba9
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 01:59:07 +0800

    chore: bump Morimens calculator asset version

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.2";
+  const assetVersion="20260922.3";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
