3634300b7da4bfb9fc7554d5e38bf24641312a6d
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 08:50:25 +0800

    chore: refresh simplified formula UI assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.97";
+  const assetVersion="20260921.98";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
