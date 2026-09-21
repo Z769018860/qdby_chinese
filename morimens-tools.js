7cb2c2623dc3b18d0b0cb7d0799611b3d8dae12f
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 22:14:52 +0800

    chore: refresh merged D-Zone usage data

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.150";
+  const assetVersion="20260921.151";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
