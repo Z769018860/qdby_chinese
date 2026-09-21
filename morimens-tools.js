7b4b8be6963595bc3df3fa023652c0ee9a797198
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 07:44:34 +0800

    chore: refresh calculator runtime fix

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.22";
+  const assetVersion="20260922.23";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
