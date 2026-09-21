bb45359fc3899405f2d79283cc0dff6f85417326
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 20:13:36 +0800

    chore: refresh love ranking assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.139";
+  const assetVersion="20260921.140";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
