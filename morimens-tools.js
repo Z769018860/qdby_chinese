4366507483326ded52d4e180fd93432af2886567
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 06:16:48 +0800

    chore: bump scoped wheel calculator cache

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.15";
+  const assetVersion="20260922.16";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
