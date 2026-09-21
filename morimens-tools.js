aa491a90d0ccde5d42a63960102e110f40cbf938
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 21:45:05 +0800

    chore: refresh unified Rouse calculator state

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.148";
+  const assetVersion="20260921.149";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
