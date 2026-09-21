af51a6702614e85327e684114f91e25f4fc0e3f6
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 06:02:10 +0800

    chore: bump D-Zone data cache for 20260922

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.14";
+  const assetVersion="20260922.15";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
