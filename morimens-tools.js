049cb1775bb0b2bef2d3bfcf019dfa5b77a0d3cb
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-21 21:35:00 +0800

    chore: refresh battle-growth calculator assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260921.146";
+  const assetVersion="20260921.147";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
