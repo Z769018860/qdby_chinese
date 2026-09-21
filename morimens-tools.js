bd1a94906d6f0756d32f74baa436342dbcb9e5f6
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 04:46:07 +0800

    chore: refresh dimensional image UI assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.11";
+  const assetVersion="20260922.12";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
