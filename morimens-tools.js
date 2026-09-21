f0702f87534a9f2f21ae49284d280a10cf137c76
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 02:02:47 +0800

    chore: bump Morimens calculator cache after state fixes

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.3";
+  const assetVersion="20260922.4";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
