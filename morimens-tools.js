26c9f67b745501d82c39fdfca5b100ecb029c212
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-22 05:15:21 +0800

    chore(morimens): bump 24 calculator audit assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.13";
+  const assetVersion="20260922.14";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
