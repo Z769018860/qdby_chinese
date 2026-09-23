8b312ef6eb4f48b701a8b3a518e4b17ba92426c1
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-24 01:49:46 +0800

    chore(i18n): bump cache for English adaptation release

--- morimens-tools.js
+++ morimens-tools.js
@@ -43,7 +43,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260924.5";
+  const assetVersion="20260924.6";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
