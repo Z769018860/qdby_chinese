d96bae6ec7283e6e171c5bec99a9ffed9ce06093
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-24 00:38:58 +0800

    chore(i18n): refresh bilingual assets

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260922.25";
+  const assetVersion="20260924.1";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
