7f12d08fcc5fffab5f465767b762ed76a4dcfab9
King Cat <33980402+Z769018860@users.noreply.github.com>
2026-09-24 00:47:08 +0800

    chore: bump Morimens asset cache version

--- morimens-tools.js
+++ morimens-tools.js
@@ -36,7 +36,7 @@ setupMorimensMascotToggle();
 
 (async()=>{
   try{
-  const assetVersion="20260924.1";
+  const assetVersion="20260924.2";
     window.MorimensDtideRenderer="legacy";
     const urls=[
       "morimens-v03/part1.b64",
